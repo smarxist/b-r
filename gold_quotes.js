1d36e8adffc1ebfc64c16bc3ad4bc1ba2b8a75ba
smarxist <60284131+smarxist@users.noreply.github.com>
2020-01-25 09:22:48 -0500

    Update gold_quotes.js

--- gold_quotes.js
+++ gold_quotes.js
@@ -9,17 +9,18 @@
 // @grant        none
 // ==/UserScript==
 
+JavaScript:
 (function() {
     'use strict';
 
     // Your code here...
     JavaScript:
 var user = document.querySelector(".p-navgroup-linkText").innerHTML;
-
+ 
 var quotes = document.querySelectorAll(".bbCodeBlock.bbCodeBlock--expandable.bbCodeBlock--quote");
-
+ 
 for(var i=0; i< quotes.length; i++){
-  if (quotes[i].querySelector('.bbCodeBlock-sourceJump').innerHTML == user + ' said:') {
+  if (quotes[i] && quotes[i].querySelector('.bbCodeBlock-title') && quotes[i].querySelector('.bbCodeBlock-title').innerHTML.includes(user + ' said:')) {
       quotes[i].style.backgroundColor = '#fffeba ';
   }
 }
